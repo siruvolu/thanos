2d23126e34255bc2468ba91e5947b3e85f9ce667
Sai <siruvolu@icloud.com>
2022-01-17 20:04:07 -0500

    UI Changes
    Added Ingredients as ListItems

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import React from 'react'
+import { IngredientItem } from './IngredientItem'
+
+const ingredients: Ingredient[] = [
+    {
+        text: 'Garlic',
+        selected: True,
+    },
+    {
+        text: 'Ginger',
+        selected: True,
+    },
+    {
+        text: 'Corriander',
+        selected: false,
+    },       
+];
+
 function App() {
     return (
-        <div>
-            <p>Test</p>
-        </div>
-    )
+        <ul>
+        <IngredientItem ingredient={ingredients[0]} />
+        <IngredientItem ingredient={ingredients[1]} />
+        <IngredientItem ingredient={ingredients[2]} />
+        </ul>
+    );
 }
 
 export default App
