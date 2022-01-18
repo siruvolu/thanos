e7e647898b5c8547bdf7366949d5442180009bca
tejakotha <teja0122@gmail.com>
2022-01-17 21:13:36 -0500

    added checkbox

--- src/components/IngredientItem/IngredientItem.tsx
+++ src/components/IngredientItem/IngredientItem.tsx
@@ -1,9 +1,23 @@
+import React, { useState } from "react";
 import { Ingredient } from "../../interfaces/Ingredient"
 
 function IngredientItem(props: { ingredient: Ingredient }) {
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = () => {
+        console.log(!checked);
+        setChecked(!checked);
+    }
     return (
         <div>
+            <input 
+            type="checkbox"
+            checked={checked}
+            onChange={handleChange}
+             />
             <span>{props.ingredient.text}</span>
+            <br/>
+            <span>Is {props.ingredient.text} selected:{"" + checked}</span>
         </div>
     )
 }
