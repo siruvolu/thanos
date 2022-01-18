7b6b2c0e1c0552d66e66436187d32b6c5e58ba7a
Sai <siruvolu@icloud.com>
2022-01-17 20:14:35 -0500

    Fixing build failure

--- src/IngredientItem.tsx
+++ src/IngredientItem.tsx
@@ -7,8 +7,10 @@ interface Props {
 export cont IngredientItem: React.FC<props> = (props) => {
     return ( 
       <li>
-        <label style={{ textDecoration: ingredient.selected ? 'line-through' : undefined }} >
-        <input type="checkbox" checked={ingredient.selected} /> {ingredient.text}
+        <label 
+         style={{ textDecoration: ingredient.selected ? 'line-through' : undefined }} 
+         >
+          <input type="checkbox" checked={ingredient.selected} /> {ingredient.text}
         </label>
       </li>
       );
